f60d9ff815186ed88a76dbe9230fe041d7df6142
Ana Beatriz Figueiredo <anabiafigue01@gmail.com>
2024-06-15 11:27:07 -0400

    exercicio

---
+++ atividade1.py
@@ -0,0 +1,7 @@
+nota = float(input("digite sua nota: "))
+
+if nota >=7:
+  print('Você foi aprovada!')
+
+else:
+ print('Você foi reprovada!')
---
+++ atividade2.py
@@ -0,0 +1,7 @@
+nota = int(input("Insira sua nota de 1 a 10: "))
+frequencia = float(input("Insira sua frequência: "))
+if nota >= 7 and frequencia >= 7.5:
+  print("Você foi aprovada!")
+else:
+  print ("Você foi reprovada!")
+  
